5ddba8aa217bfa3943d9a659438e7465db70a087
litao9595 <46311649+litao9595@users.noreply.github.com>
2020-12-21 13:23:43 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,12 +1,67 @@
-var hot="http://10.143.132.203:8087";
-var orgId=1;
-var appId="222222";
-var getUrl={
-    findCourseSquarePageList:hot+'/core-course/a/H5/course/findCourseSquarePageList',//课程广场接口(包含精品课程和更多课程)
-    findCourseDetail:hot+'/core-course/a/H5/course/findCourseDetail',//查询课程详细数据
-    findCourseChapterList:hot+'/core-course/a/H5/course/findCourseChapterList',//查询课程大纲(课程章节)列表数据
-    findCourseTeacherList:hot+'/core-course/a/H5/course/findCourseTeacherList',//查询课程老师列表数据
-    findCourseEvaluateList:hot+'/core-course/a/H5/course/findCourseEvaluateList',//学生评价课程
-    findHomeworkPageList:hot+'/core-homework/a/H5/homework/myStudentHomework',//学生查询我的作业列表数据
-};
+//滚动加载更多
+function LoadMore(fn,obj){
+    var timeoutInt;
+    var _this=this;
+    this.obj=obj;
+    this.jonscroll=function(){
+        window.onscroll = function () {
+            if (timeoutInt != undefined) {
+                window.clearTimeout(timeoutInt);
+            }
+            timeoutInt=function(){
+                if(_this.getScrollHeight() == _this.getDocumentTop() + _this.getWindowHeight()){
+                    //当滚动条到底时,这里是触发内容
+                    fn(_this.obj)
+                }
+            }();
+        }
+    };
+    this.jonscroll();
+    //文档高度
+    this.getDocumentTop=function(){
+        var scrollTop =  0, bodyScrollTop = 0, documentScrollTop = 0;
+        if (document.body) {
+            bodyScrollTop = document.body.scrollTop;
+        }
+        if (document.documentElement) {
+            documentScrollTop = document.documentElement.scrollTop;
+        }
+        scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;
+        return scrollTop;
+    };
+    //可视窗口高度
+    this.getWindowHeight=function(){
+        var windowHeight = 0;
+        if (document.compatMode == "CSS1Compat") {
+            windowHeight = document.documentElement.clientHeight;
+        } else {
+            windowHeight = document.body.clientHeight;
+        }
+        return windowHeight;
+    };
+    //滚动条滚动高度
+    this.getScrollHeight=function(){
+        var scrollHeight = 0, bodyScrollHeight = 0, documentScrollHeight = 0;
+        if (document.body) {
+            bodyScrollHeight = document.body.scrollHeight;
+        }
+        if (document.documentElement) {
+            documentScrollHeight = document.documentElement.scrollHeight;
+        }
+        scrollHeight = (bodyScrollHeight - documentScrollHeight > 0) ? bodyScrollHeight : documentScrollHeight;
+        return scrollHeight;
+    };
+}
+
+var ks=new LoadMore(function(obj){
+},{pageNo:1,pageSize:10});
+
+
+//获取地址栏参数
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+}
 
