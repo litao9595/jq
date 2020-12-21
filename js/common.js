7d1632ab42a70bb231f2b5d94c55c80eb4977059
litao9595 <46311649+litao9595@users.noreply.github.com>
2020-12-21 13:21:21 +0800

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,12 @@
+var hot="http://10.143.132.203:8087";
+var orgId=1;
+var appId="222222";
+var getUrl={
+    findCourseSquarePageList:hot+'/core-course/a/H5/course/findCourseSquarePageList',//课程广场接口(包含精品课程和更多课程)
+    findCourseDetail:hot+'/core-course/a/H5/course/findCourseDetail',//查询课程详细数据
+    findCourseChapterList:hot+'/core-course/a/H5/course/findCourseChapterList',//查询课程大纲(课程章节)列表数据
+    findCourseTeacherList:hot+'/core-course/a/H5/course/findCourseTeacherList',//查询课程老师列表数据
+    findCourseEvaluateList:hot+'/core-course/a/H5/course/findCourseEvaluateList',//学生评价课程
+    findHomeworkPageList:hot+'/core-homework/a/H5/homework/myStudentHomework',//学生查询我的作业列表数据
+};
+
